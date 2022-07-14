b7cc29315d4c7801d7d30b7722a415572836ef36
Adam Dobeš <xdobes3@fi.muni.cz>
2022-07-14 19:34:37 +0200

    Add advanced functions

--- script.js
+++ script.js
@@ -1,26 +1,100 @@
 function generateDivs(count) {
-    let size = 100 / count;
-    
+    if (count > 100) {
+        console.log("Maximum is 100x100 pixels");
+        return;
+    }
     const drawArea = document.querySelector(".drawing");
+    drawArea.style.gridTemplateColumns = `repeat(${count}, 1fr)`;
+    drawArea.style.gridTemplateRows = `repeat(${count}, 1fr)`;
 
     for (let i = 0; i < count * count; i++) {
         let pixel = document.createElement("div");
         pixel.classList.add("pixel");
-        pixel.style.width = `${size}%`;
-        pixel.style.height = `${size}%`;
         drawArea.appendChild(pixel);
     }
 }
 
 
+function draw() {
+    let color = "black";
+    let drag = false;
+    let rainbow = false;
+    let eraser = false; 
 
-generateDivs(20);
-const pixels = document.querySelectorAll(".pixel");
-pixels.forEach( pixel => {
-    pixel.addEventListener("mouseover", () => pixel.classList.add("active"));
-});
+    const clearBtn = document.querySelector("#clear");
+    const colorBtn = document.querySelector("#color");
+    const eraserBtn = document.querySelector("#eraser");
+    const rainbowBtn = document.querySelector("#rainbow");
+    const pixels = document.querySelectorAll(".pixel");
+    
+    document.addEventListener("mousedown", () => drag = true);
+    document.addEventListener("mouseup", () => drag = false);
+
+    clearBtn.addEventListener("click", () => clear(pixels));
+    eraserBtn.addEventListener("click", () => eraser = !eraser);
+    rainbowBtn.addEventListener("click", () => {
+        eraser = false;
+        rainbow = !rainbow;
+    });
+
+    ["click", "change"].forEach(evt => {
+        colorBtn.addEventListener(evt, () => {
+            color = colorBtn.value;
+            rainbow = false;
+            eraser = false;
+        });
+    });
+
+    for (const pixel of pixels) {
+        pixel.addEventListener("mouseover", () => {
+            if (drag) setPixelColor(pixel, rainbow, eraser, color);
+        });
+    }
+}
 
 
+function setPixelColor(pixel, rainbow, eraser, color) {
+    if (eraser) {
+        color = "white";
+    } else if (rainbow) {
+        color = getRandomColor();
+    }
+    pixel.style.backgroundColor = color;
+}
 
-// let count = Number(prompt("Enter how many pixels"));
-// console.log(count);
+
+function clear(pixels) {
+    pixels.forEach(pixel =>{
+        pixel.style.backgroundColor = "white";
+    });
+}
+
+
+function getRandomColor() {    
+    let color = "#"
+    const possible = "0123456789ABCDEF";
+    for (let i = 0; i< 6; i++) {
+        color += possible[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function removePixels() {
+    const pixels = document.querySelectorAll(".pixel");
+    for (let pixel of pixels) {
+        pixel.remove();
+    }
+}
+function main() {
+    const slider = document.querySelector(".slider");
+    generateDivs(Number(slider.value));
+    draw();
+
+    slider.addEventListener("input", () => {
+        removePixels();
+        generateDivs(Number(slider.value));
+        draw();
+    });
+}
+
+main();
